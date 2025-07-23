2a81b7698d4042da090bb93c5d6dd2141eecd9aa
Ibtihel <ibtihelnemri1@gmail.com>
2025-07-23 19:25:54 +0000

    Finalisation de la pipeline DVC avec params.yaml

--- src/models/grid_search.py
+++ src/models/grid_search.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import pickle
+import yaml
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
 
 X_train = pd.read_csv("data/processed/X_train_scaled.csv")
 y_train = pd.read_csv("data/processed/y_train.csv").values.ravel()
 
+with open("params.yaml", "r") as f:
+    params = yaml.safe_load(f)
+
 param_grid = {
-    'n_estimators': [50, 100],
-    'max_depth': [3, 5],
-    'learning_rate': [0.05, 0.1]
+    'n_estimators': params['gridsearch']['n_estimators'],
+    'max_depth': params['gridsearch']['max_depth'],
+    'learning_rate': params['gridsearch']['learning_rate']
 }
 
 grid = GridSearchCV(GradientBoostingRegressor(), param_grid, cv=3, scoring='r2')
